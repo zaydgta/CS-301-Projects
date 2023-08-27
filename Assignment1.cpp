210b3f0b15cd59d042395973f8a70bb9e28c127e
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-08-27 14:39:52 -0700

    Update Assignment1.cpp
    Assignment1.cpp updated with class function definitions

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,11 +1,53 @@
 // Assignment1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Number_bucket.h"
 
-int main()
-{
-    cout << "Testing formatting" << endl << "This should be on the second line" << endl << "Test complete";
-    return 0;
+
+	
+// Constructor
+Number_bucket::Number_bucket() {
+
+};
+
+// Destructor
+Number_bucket::~Number_bucket() {
+
+};
+
+// Checks whether or not the stack is empty
+bool Number_bucket::isEmpty() const {
+	return 0;
+};
+
+// Adds an integer into the stack.
+void Number_bucket::push(int n) {
+	
+};
+
+// Removes the integer from the top of the stack.
+void Number_bucket::pop() {
+	
+};
+
+// Shows the number of elements currently added to the stack.
+int Number_bucket::get_size() const {
+	
+};
+
+// Return the int that is currently at the top of the stack.
+int Number_bucket::top() const {
+	
+};
+    
+int main() {
+	
+	stack<int> bucket;
+	bucket.push(5);
+	bucket.top();
+	//bucket.get_size();
+	bucket.pop();
+	bucket.top();
+
+
+	return 0;
 }
