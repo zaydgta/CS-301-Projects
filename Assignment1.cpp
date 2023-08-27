b1a34de259760d66e0b7810e624eb3c4c179c1c9
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-08-27 15:08:46 -0700

    Update Assignment1.cpp
    More function updates

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -2,11 +2,10 @@
 
 #include "Number_bucket.h"
 
-
-	
 // Constructor
 Number_bucket::Number_bucket() {
-
+	m = 0;
+	bucket[100] = { NULL };
 };
 
 // Destructor
@@ -16,37 +15,52 @@ Number_bucket::~Number_bucket() {
 
 // Checks whether or not the stack is empty
 bool Number_bucket::isEmpty() const {
-	return 0;
+	
+	if (bucket[0] == NULL) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Adds an integer into the stack.
 void Number_bucket::push(int n) {
-	
+
+	bucket[m] = n;
+	m++;
+
 };
 
 // Removes the integer from the top of the stack.
 void Number_bucket::pop() {
+	
+	bucket[m] = NULL;
+	m--;
 	
 };
 
 // Shows the number of elements currently added to the stack.
 int Number_bucket::get_size() const {
 	
+	return m;
+
 };
 
 // Return the int that is currently at the top of the stack.
 int Number_bucket::top() const {
 	
+	return bucket[m];
+
 };
     
 int main() {
 	
-	stack<int> bucket;
-	bucket.push(5);
-	bucket.top();
+	//stack<int> bucket;
+	//bucket.push(5);
+	//bucket.top();
 	//bucket.get_size();
-	bucket.pop();
-	bucket.top();
+	//bucket.pop();
+	//bucket.top();
 
 
 	return 0;
