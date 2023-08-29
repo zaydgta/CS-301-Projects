a0d82884031099aa1ef9371f220c6bcffe945ba0
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-08-29 14:24:18 -0700

    Update Assignment1.cpp
    Added file input and output functions into the cpp file

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -4,7 +4,7 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Work to be done
-// Added a file input and output using <fstream>
+// Check if the input and output functions are working
 // Create a test driver.cpp
 // Create more constructors to include file inputs
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -35,40 +35,60 @@ void Number_bucket::push(int n) {
 
 	bucket[m] = n;
 	m++;
-
 };
 
 // Removes the integer from the top of the stack.
 void Number_bucket::pop() {
 	
 	bucket[m] = NULL;
-	m--;
-	
+	m--;	
 };
 
 // Shows the number of elements currently added to the stack.
 int Number_bucket::get_size() const {
 	
 	return m;
-
 };
 
 // Return the int that is currently at the top of the stack.
 int Number_bucket::top() const {
 	
 	return bucket[m];
-
 };
     
 int main() {
-	
-	//stack<int> bucket;
-	//bucket.push(5);
-	//bucket.top();
-	//bucket.get_size();
-	//bucket.pop();
-	//bucket.top();
 
+	// Variables
+	ifstream inputFile;
+	ofstream outputFile:
+	int n;
+	Number_bucket bucket;
 
+	// Opens the input file
+	inputFile.open("testDataInput.txt");
+
+	// Reads the data into the variable n then adds it into the stack
+	while (inputFile.is_open()){
+		inputFile >> n;
+		bucket.push(n);
+	}
+
+	// Closes the input file
+	inputFile.close();
+
+	// All test cases to be implemented
+
+	// Opens the output file;
+	outputFile.open("testDataOutput.txt");
+
+	// Outputs the data into the output file
+	while (outputFile.is_open()){
+		outputFile << n << endl;
+	}
+
+	// Closes the output file
+	outputFile.close();
+
+	// Ends the program
 	return 0;
 }
