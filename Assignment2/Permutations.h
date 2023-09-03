75e6b9fe3134c59513dad5ccbdb9c7328015c4a1
hasibayoubi <82474817+hasibayoubi@users.noreply.github.com>
2023-09-03 14:10:31 -0700

    Add files via upload

--- Assignment2/Permutations.h
+++ Assignment2/Permutations.h
@@ -1,52 +1,28 @@
 #ifndef PERMUTATIONS_H
 #define PERMUTATIONS_H
 
-#include <iostream>
 #include <string>
-#include <cstring>
-#include <fstream>
 #include <vector>
-using namespace std;
+#include <fstream>
 
 class Permutations {
+public:
+    Permutations(int n);
+    ~Permutations();
+
+    void GeneratePermutations();
+    void PrintPermutations();
+    int getFactorial(int n);
 
 private:
+    int setSize;
+    std::vector<int> permutation;
+    std::vector<int> direction;
 
-	// Variables
-	int n, k;
-	vector<int> num;
-
-public:
-
-	// Constructor
-	Permutations();
-
-	// Constructor for file input
-	Permutations(std::string inputFileName);
-
-	// Destructor
-	~Permutations();
-
-	// Sorts the vector using bubbleSort algorithm
-	void bubbleSort();
-
-	// Swaps the two integers
-	void swap(int a, int b);
-
-	// Adds an integer to the vector.
-	void add(int a, int b);
-
-	// Removes an integer from the vector.
-	void remove(int a);
-
-	// Shows the number of elements currently in the vector.
-	int getSize() const;
-
-	// Swaps all integers to get every permutation
-	void allPermutations();
-
-	// Prints the vectors permutations
-	void printPermutations();
+    bool FindMobileElement(int& mobileIndex);
+    void Swap(int index1, int index2);
+    void ReverseDirections(int k);
+    void InitializeSet();
 };
 
-#endif
+#endif // PERMUTATIONS_H
