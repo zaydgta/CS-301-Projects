7e03aaa50ac05d5d4239d1b74a290838622fd465
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-09-05 14:02:08 -0700

    Update and rename Assignment2.cpp to Permutations.cpp
    Made Permutations.cpp solely an implementation file for Permutations.h

--- Assignment2/Permutations.cpp
+++ Assignment2/Permutations.cpp
@@ -104,29 +104,3 @@ int Permutations::getFactorial(int n) {
         return n * getFactorial(n - 1);
     }
 }
-
-// Main function
-int main() {
-    
-    // Variables
-    int n;
-
-    // Prints a message to the user
-    cout << "Enter n (1 <= n <= 25): ";
-
-    // Asks user for input
-    cin >> n;
-
-    // Checks for valid input
-    while (n < 1 || n > 25) {
-        cout << "Invalid input. n must be between 1 and 25." << endl;
-        cin >> n;
-    }
-
-    // Creates a permutations object
-    Permutations p(n);
-    p.GeneratePermutations();
-
-    // Ends program
-    return 0;
-}
