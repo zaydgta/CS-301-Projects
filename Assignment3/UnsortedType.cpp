c443a3d1b7b304e66a95568ee95da765fbfb8fd5
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-09-07 13:52:02 -0700

    Update UnsortedType.cpp

--- Assignment3/UnsortedType.cpp
+++ Assignment3/UnsortedType.cpp
@@ -1,74 +1,69 @@
 // This file contains the linked implementation of class
 // UnsortedType.
 
-#include "unsorted.h"
-struct NodeType
-{
+#include "UnsortedType.h"
+struct NodeType {
     ItemType info;
     NodeType* next;
 };
 
-UnsortedType::UnsortedType()  // Class constructor
-{
+// Class constructor
+UnsortedType::UnsortedType() {
   length = 0;
   listData = NULL;
 }
-bool UnsortedType::IsFull() const
+
 // Returns true if there is no room for another ItemType 
 //  on the free store; false otherwise.
-{
+bool UnsortedType::IsFull() const {
   NodeType* location;
-  try
-  {
+  try {
     location = new NodeType;
     delete location;
     return false;
-  }
-  catch(std::bad_alloc exception)
-  {
+  } catch(std::bad_alloc exception) {
     return true;
   }
 }
 
-int UnsortedType::GetLength() const
 // Post: Number of items in the list is returned.
-{
+int UnsortedType::GetLength() const {
   return length;
 }
 
-void UnsortedType::MakeEmpty()
 // Post: List is empty; all items have been deallocated.
-{
-  NodeType* tempPtr;
+void UnsortedType::MakeEmpty(){
+  
+	NodeType* tempPtr;
 
-    while (listData != NULL)
-    {
-      tempPtr = listData;
-      listData = listData->next;
-      delete tempPtr;
-  }
-  length = 0;
+	while (listData != NULL) {
+      		tempPtr = listData;
+      		listData = listData->next;
+      		delete tempPtr;
+  	}
+  	
+	length = 0;
 }
-void UnsortedType::PutItem(ItemType item)
+
 // item is in the list; length has been incremented.
-{
-  NodeType* location;			// Declare a pointer to a node
+void UnsortedType::PutItem(ItemType item) {
+  
+	NodeType* location;		// Declare a pointer to a node
 
-  location = new NodeType;		// Get a new node 
-  location->info = item;		// Store the item in the node
-  location->next = listData;	// Store address of first node 
-						//   in next field of new node
-  listData = location;		// Store address of new node into
-						//   external pointer
-  length++;				// Increment length of the list
+  	location = new NodeType;	// Get a new node 
+  	location->info = item;		// Store the item in the node
+  	location->next = listData;	// Store address of first node 
+					//   in next field of new node
+  	listData = location;		// Store address of new node into
+					//   external pointer
+  	length++;			// Increment length of the list
 }
 
-ItemType UnsortedType::GetItem(ItemType& item, bool& found)
 // Pre:  Key member(s) of item is initialized.
 // Post: If found, item's key matches an element's key in the 
 //       list and a copy of that element has been stored in item;
 //       otherwise, item is unchanged. 
-{
+ItemType UnsortedType::GetItem(ItemType& item, bool& found) {
   bool moreToSearch;
   NodeType* location;
 
@@ -76,10 +71,8 @@ ItemType UnsortedType::GetItem(ItemType& item, bool& found)
   found = false;
   moreToSearch = (location != NULL);
 
-  while (moreToSearch && !found) 
-  {
-    switch (item.ComparedTo(location->info))
-    {
+  while (moreToSearch && !found) {
+    switch (item.ComparedTo(location->info)){
       case LESS    : 
       case GREATER : location = location->next;
                      moreToSearch = (location != NULL);
@@ -92,22 +85,19 @@ ItemType UnsortedType::GetItem(ItemType& item, bool& found)
   return item;
 }
 
- void UnsortedType::DeleteItem(ItemType item)
 // Pre:  item's key has been initialized.
 //       An element in the list has a key that matches item's.
 // Post: No element in the list has a key that matches item's.
-{
-  NodeType* location = listData;
-  NodeType* tempLocation;
+void UnsortedType::DeleteItem(ItemType item) {
+  
+	NodeType* location = listData;
+  	NodeType* tempLocation;
 
   // Locate node to be deleted.
-  if (item.ComparedTo(listData->info) == EQUAL)
-  {
+  if (item.ComparedTo(listData->info) == EQUAL) {
     tempLocation = location;
     listData = listData->next;		// Delete first node.
-  }
-  else
-  {
+  } else {
     while (item.ComparedTo((location->next)->info) != EQUAL)
       location = location->next;
 
@@ -119,17 +109,15 @@ ItemType UnsortedType::GetItem(ItemType& item, bool& found)
   length--;
 }
 
-void UnsortedType::ResetList()
 // Post: Current position has been initialized.
-{
+void UnsortedType::ResetList() {
   currentPos = NULL;
 }
- 
-ItemType UnsortedType::GetNextItem()
+
 // Post:  A copy of the next item in the list is returned.
 //        When the end of the list is reached, currentPos
 //        is reset to begin again.
-{
+ItemType UnsortedType::GetNextItem(){
   ItemType item;
   if (currentPos == NULL)
     currentPos = listData;
@@ -139,9 +127,8 @@ ItemType UnsortedType::GetNextItem()
   return item;
 }
 
-UnsortedType::~UnsortedType()
 // Post: List is empty; all items have been deallocated.
-{
+UnsortedType::~UnsortedType() {
   NodeType* tempPtr;
 
   while (listData != NULL)
@@ -151,8 +138,3 @@ UnsortedType::~UnsortedType()
     delete tempPtr;
   }
 }
-
-
-
-
-
