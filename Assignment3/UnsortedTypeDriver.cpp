cf08525b9601e1bd21ddeccc1fe0fecf9d64c726
zaydgta <126204053+zaydgta@users.noreply.github.com>
2023-09-07 14:04:23 -0700

    Update UnsortedTypeDriver.cpp

--- Assignment3/UnsortedTypeDriver.cpp
+++ Assignment3/UnsortedTypeDriver.cpp
@@ -12,84 +12,84 @@ void PrintList(ofstream& outFile, UnsortedType& list);
 
 int main(){
 
-  ifstream inFile;       // file containing operations
-  ofstream outFile;      // file containing output
-  string inFileName;     // input file external name
-  string outFileName;    // output file external name
-  string outputLabel;     
-  string command;        // operation to be executed
+	// Variables
+  	ifstream inFile;       // file containing operations
+  	ofstream outFile;      // file containing output
+  	string inFileName;     // input file external name
+  	string outFileName;    // output file external name
+  	string outputLabel;     
+  	string command;        // operation to be executed
+  	int number;
+  	ItemType item;
+  	UnsortedType list;
+  	bool found;
+  	int numCommands;
+
+
+  	// Prompt for file names, read file names, and prepare files
+  	cout << "Enter name of input command file; press return." << endl;
+  	cin  >> inFileName;
+  	inFile.open(inFileName.c_str());
+
+  	cout << "Enter name of output file; press return." << endl;
+  	cin  >> outFileName;
+  	outFile.open(outFileName.c_str());
+
+  	cout << "Enter name of test run; press return." << endl;
+  	cin  >> outputLabel;
+  	outFile << outputLabel << endl;
   
-  int number;
-  ItemType item;
-  UnsortedType list;
-  bool found;
-  int numCommands;
-
-
-  // Prompt for file names, read file names, and prepare files
-  cout << "Enter name of input command file; press return." << endl;
-  cin  >> inFileName;
-  inFile.open(inFileName.c_str());
-
-  cout << "Enter name of output file; press return." << endl;
-  cin  >> outFileName;
-  outFile.open(outFileName.c_str());
-
-  cout << "Enter name of test run; press return." << endl;
-  cin  >> outputLabel;
-  outFile << outputLabel << endl;
+  	if (!inFile){
+    		cout << "file not found" << endl;
+		exit(2)
+ 	}
   
-  if (!inFile){
-    cout << "file not found" << endl;
-	  exit(2)
-  }
-  
-  inFile >> command;
+  	inFile >> command;
 
-  numCommands = 0;
-  while (command != "Quit"){ 
-    if (command == "PutItem"){
-      inFile >> number; 
-      item.Initialize(number);
-      list.PutItem(item);
-      item.Print(outFile);
-      outFile << " is in list" << endl;
-    } else if (command == "DeleteItem"){
-      inFile >> number;
-      item.Initialize(number);
-      list.DeleteItem(item);
-      item.Print(outFile);
-      outFile << " is deleted" << endl;
-    } else if (command == "GetItem") {
-      inFile >> number;
-      item.Initialize(number);
-      item = list.GetItem(item, found);
-      item.Print(outFile);
-      if (found)
-        outFile << " found in list." << endl;
-      else outFile <<  " not in list."  << endl;  
-    } else if (command == "GetLength")  
-      outFile << "Length is " << list.GetLength() << endl;
-    else if (command == "IsFull")
-      if (list.IsFull())
-        outFile << "List is full." << endl;
-      else outFile << "List is not full."  << endl;  
-    else if (command == "MakeEmpty")
-	  list.MakeEmpty();
-	else if (command == "PrintList")
-	  PrintList(outFile, list);
-	else
-	  cout << command << " is not a valid command." << endl;
-	numCommands++;
-    cout <<  " Command number " << numCommands << " completed." 
-         << endl;
-    inFile >> command;
-  };
- 
-  cout << "Testing completed."  << endl;
-  inFile.close();
-  outFile.close();
-  return 0;
+  	numCommands = 0;
+  	while (command != "Quit"){ 
+    		if (command == "PutItem"){
+      			inFile >> number; 
+      			item.Initialize(number);
+      			list.PutItem(item);
+      			item.Print(outFile);
+      			outFile << " is in list" << endl;
+    		} else if (command == "DeleteItem"){
+      			inFile >> number;
+      			item.Initialize(number);
+      			list.DeleteItem(item);
+      			item.Print(outFile);
+      			outFile << " is deleted" << endl;
+    		} else if (command == "GetItem") {
+      			inFile >> number;
+      			item.Initialize(number);
+      			item = list.GetItem(item, found);
+      			item.Print(outFile);
+      			if (found)
+        			outFile << " found in list." << endl;
+      			else outFile <<  " not in list."  << endl;  
+    		} else if (command == "GetLength")  
+      			outFile << "Length is " << list.GetLength() << endl;
+    		else if (command == "IsFull")
+      			if (list.IsFull())
+        			outFile << "List is full." << endl;
+      			else outFile << "List is not full."  << endl;  
+   		 else if (command == "MakeEmpty")
+	  		list.MakeEmpty();
+		else if (command == "PrintList")
+	  		PrintList(outFile, list);
+		else
+	  		cout << command << " is not a valid command." << endl;
+		
+		numCommands++;
+    		cout <<  " Command number " << numCommands << " completed." << endl;
+    		inFile >> command;
+ 	};
+ 	
+	cout << "Testing completed."  << endl;
+  	inFile.close();
+  	outFile.close();
+  	return 0;
 }
 
 // Pre:  list has been initialized.      
@@ -98,16 +98,16 @@ int main(){
 //       dataFile is still open.  
 void PrintList(ofstream& dataFile, UnsortedType& list) {
   
-  int length;
-  ItemType item;
+	int length;
+  	ItemType item;
 
-  list.ResetList();
-  length = list.GetLength();
-  for (int counter = 1; counter <= length; counter++) {
-    item = list.GetNextItem();
-    item.Print(dataFile);
-  }
+  	list.ResetList();
+  	length = list.GetLength();
+  	
+	for (int counter = 1; counter <= length; counter++) {
+    		item = list.GetNextItem();
+    		item.Print(dataFile);
+  	}
   
-  dataFile << endl;
+  	dataFile << endl;
 }
-
