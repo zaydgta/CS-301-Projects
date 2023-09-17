a33a5fc6dc3f3188288aec5a09f73e6b655a7cda
hasibayoubi <hasibayoubi2831@gmail.com>
2023-09-17 15:23:34 -0700

    Fixed the issue with PrintList function. Passed the list object by reference (SortedType& list)

--- Assignment4/SortedTypeDriver.cpp
+++ Assignment4/SortedTypeDriver.cpp
@@ -8,7 +8,8 @@
 #include "SortedType.h"
 using namespace std;
 
-void PrintList(ofstream& outFile, SortedType list);
+void PrintList(ofstream& outFile, SortedType& list);
+
 
 int main(){
 
@@ -16,7 +17,7 @@ int main(){
     ofstream outFile;      // file containing output
     string inFileName;     // input file external name
     string outFileName;    // output file external name
-    string outputLabel;     
+    string outputLabel;
     string command;        // operation to be executed
     int number;
     ItemType item;
@@ -40,22 +41,22 @@ int main(){
     inFile >> command;
     numCommands = 0;
 
-   while (command != "Quit"){ 
-       if (command == "PutItem") {
-           inFile >> number;
-           item.Initialize(number);
-           list.PutItem(item);
-           item.Print(outFile);
-           outFile << " is inserted" << endl;
+    while (command != "Quit"){
+        if (command == "PutItem") {
+            inFile >> number;
+            item.Initialize(number);
+            list.PutItem(item);
+            item.Print(outFile);
+            outFile << " is inserted" << endl;
 
-       } else if (command == "DeleteItem") {
+        } else if (command == "DeleteItem") {
             inFile >> number;
             item.Initialize(number);
             list.DeleteItem(item);
             item.Print(outFile);
             outFile << " is deleted" << endl;
 
-       } else if (command == "GetItem") {
+        } else if (command == "GetItem") {
             inFile >> number;
             item.Initialize(number);
             list.GetItem(item, found);
@@ -65,46 +66,47 @@ int main(){
                 outFile << number << " not in list." << endl;
             }
 
-       } else if (command == "GetLength") {
-           outFile << "Length is " << list.GetLength() << endl;
-       } else if (command == "IsFull") {
-           if (list.IsFull()) {
-               outFile << "List is full." << endl;
-           } else {
-               outFile << "List is not full." << endl;
-           }
-       } else if (command == "MakeEmpty") {
+        } else if (command == "GetLength") {
+            outFile << "Length is " << list.GetLength() << endl;
+        } else if (command == "IsFull") {
+            if (list.IsFull()) {
+                outFile << "List is full." << endl;
+            } else {
+                outFile << "List is not full." << endl;
+            }
+        } else if (command == "MakeEmpty") {
             list.MakeEmpty();
-       } else if (command == "PrintList") {
+            outFile << "List is empty." << endl;
+        } else if (command == "PrintList") {
             PrintList(outFile, list);
-       } else {
-           cout << "Command not recognized." << endl;
-       }
+        } else {
+            cout << "Command not recognized." << endl;
+        }
 
         numCommands++;
         cout <<  " Command number " << numCommands << " completed." << endl;
         inFile >> command;
-  };
- 
+    };
+
     cout << "Testing completed."  << endl;
     inFile.close();
     outFile.close();
     return 0;
 }
 
-// Pre:  list has been initialized.      
-//       dataFile is open for writing.   
+// Pre:  list has been initialized.
+//       dataFile is open for writing.
 // Post: Each component in list has been written to dataFile.
-//       dataFile is still open.   
-void PrintList(ofstream& dataFile, SortedType list) {
-  
+//       dataFile is still open.
+void PrintList(ofstream& dataFile, SortedType& list) {
+
     int length;
     ItemType item;
 
     list.ResetList();
     length = list.GetLength();
     for (int counter = 1; counter <= length; counter++){
-    
+
         ItemType item;
         item = list.GetNextItem();
         item.Print(dataFile);
